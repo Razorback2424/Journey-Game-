d1d4a6231a52f5abb4e3491ff8437785d703bccf
Razorback2424 <34964901+Razorback2424@users.noreply.github.com>
2026-08-03 17:22:07 -0600

    Add Source/JourneyGame/Private/Core/JGLog.cpp

---
+++ Source/JourneyGame/Private/Core/JGLog.cpp
@@ -0,0 +1,7 @@
+#include "Core/JGLog.h"
+
+DEFINE_LOG_CATEGORY(LogJourney);
+DEFINE_LOG_CATEGORY(LogJourneyOverworld);
+DEFINE_LOG_CATEGORY(LogJourneyEncounter);
+DEFINE_LOG_CATEGORY(LogJourneyInteraction);
+
