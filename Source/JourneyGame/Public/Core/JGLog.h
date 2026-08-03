deee4704293f6aba66902c58439b6945b8f1a78d
Razorback2424 <34964901+Razorback2424@users.noreply.github.com>
2026-08-03 17:24:03 -0600

    Add Source/JourneyGame/Public/Core/JGLog.h

---
+++ Source/JourneyGame/Public/Core/JGLog.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "CoreMinimal.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(LogJourney, Log, All);
+DECLARE_LOG_CATEGORY_EXTERN(LogJourneyOverworld, Log, All);
+DECLARE_LOG_CATEGORY_EXTERN(LogJourneyEncounter, Log, All);
+DECLARE_LOG_CATEGORY_EXTERN(LogJourneyInteraction, Log, All);
+
